5b50de64f7d257c355c97ef91b3901b0953641c4
egonreinhardt <egonreinhardt@hotmail.com>
2023-04-06 20:11:12 -0300

    Avanços no desenvolvimento da Action Bar: Popup menu (Flutuante) na direita com item que leva à tela inicial, ainda não-funcional. E ajustes no seu visual.

--- app/src/main/java/com/example/jokenpo/PlayerActivity.kt
+++ app/src/main/java/com/example/jokenpo/PlayerActivity.kt
@@ -2,6 +2,8 @@ package com.example.jokenpo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.example.jokenpo.databinding.ActivityPlayerBinding
 
 //Activity que representa a Tela 02 (Jogador).
@@ -16,5 +18,22 @@ class PlayerActivity : AppCompatActivity() {
         setSupportActionBar(toolbar) //AppBar
 
         supportActionBar?.title = "Jokenpo" //Appbar
+    }
+
+    //Define que o Action Bar que aparecerá na tela do app é o nosso action_bar.xml
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.action_bar,menu)
+        return true
+    }
+
+    //Define a função do botão dentro do menu flutuante (3 pontinhos) da Action Bar
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.action_bar_home -> {
+                //ADICIONAR AQUI A FUNÇÃO DE LEVAR À HOME.
+                true
+            }
+            else -> false
+        }
     }
 }
